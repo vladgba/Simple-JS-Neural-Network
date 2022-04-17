f4be17eaa53b5ed82b1c6e1d0f0d8155689ff818
vladgba <vlad.gba@gmail.com>
2022-04-17 14:19:46 +0300

    some fixes & code style

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let brain;
-function setup(){
-    brain = new NeuralNetwork([2,2]);
+function setup() {
+    brain = new NeuralNetwork([2, 2]);
     
-    for(let i = 0; i < 10000 ; i++ ){
+    for (let i = 0; i < 10000; i++) {
         let num1 = Math.random();
         let num2 = Math.random();
-        brain.train([num1,num2], (num1>num2?[1,0]:[0,1]));
+        brain.train([num1, num2], (num1 > num2 ? [1, 0] : [0, 1]));
     }
 }
 
